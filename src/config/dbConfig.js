ca587cf056210474831fcbba0289b63110253be9
Walter <aguirrewalterjesus@gmail.com>
2024-06-03 19:56:52 -0300

    Cambios en dbConfig

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Root123',
+  password: '@Localhost23',
   database: 'idw',
   waitForConnections: true,
   connectionLimit: 10,
